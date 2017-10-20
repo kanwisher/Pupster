97ad9ab3f6bbf01741ba2d41eac6ebb6fa00bc99
David Kanwisher <kanwisher@gmail.com>
2017-10-19 23:58:08 -0400

    style for about page

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+const style = {
+    banner: {
+        backgroundImage: "url('https://i.imgur.com/qkdpN.jpg')",
+        height: "500px",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat"
+    }
+}
+
+
 
 const About = () =>(
     <div>
-        <h1> About </h1>
+        <h1 style ={style.banner}> About </h1>
         <p> Welcome to the beastiality page </p>
         <p> Find a dog date to meet your fetish needs</p>
     </div>
